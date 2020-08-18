28743c07a4aadd299c68568fe374bd8487fba0d9
Mohammed Sadiq <sadiqmmm@gmail.com>
2020-08-18 14:12:36 +0530

    Add the audio assets and play note1.wav

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:audioplayers/audioplayers.dart';
+import 'package:audioplayers/audio_cache.dart';
 
 void main() {
   runApp(MyApp());
@@ -21,6 +22,8 @@ class MyXylophone extends StatefulWidget {
 }
 
 class _MyXylophoneState extends State<MyXylophone> {
+  final player = AudioCache();
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -29,7 +32,12 @@ class _MyXylophoneState extends State<MyXylophone> {
         centerTitle: true,
       ),
       body: Center(
-        child: Text("Welcome"),
+        child: FlatButton(
+          onPressed: () {
+            player.play("note1.wav");
+          },
+          child: Text("play"),
+        ),
       ),
     );
   }
